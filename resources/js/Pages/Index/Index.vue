52796efb8cee8cfda9cb2c9773dc07b3e595ca9f
maisy <maisy.ltnl@kolaogroup.com>
2024-08-11 13:58:45 +0700

    update

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,25 +1,16 @@
 <template>
-    <MainLayout>
-        <div class="text-red-500">Hello!</div>
+    <div class="text-red-500">Hello!</div>
 
-        <div>
-            Them messageis {{ message }}
-        </div>
-    </MainLayout>
-
+    <div>
+        Them messageis {{ message }}
+    </div>
 </template>
 
 
-
-
 <script setup>
-
-import { Link } from '@inertiajs/vue3';
-import MainLayout from '../../Layouts/MainLayout.vue';
 defineProps(
     {
         message: String
     }
 )
-
 </script>
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,13 +1,9 @@
 <template>
-    <MainLayout>
-        <div>
-            Show test
-
-        </div>
-    </MainLayout>
+    <div>
+        Show test
+        <Link>Link</Link>
+    </div>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3';
-import MainLayout from '../../Layouts/MainLayout.vue';
 </script>
