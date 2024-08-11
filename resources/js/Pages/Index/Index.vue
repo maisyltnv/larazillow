a75268ed3f2921d1b9db1c6b9d4b86a91699bd4c
maisy <maisy.ltnl@kolaogroup.com>
2024-08-11 11:20:05 +0700

    update

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>Hello!</div>
+    <div>Counter: {{ counter }}</div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  
+  const counter = ref(1)
+  
+  setInterval(() => {
+    counter.value++
+  }, 1000)
+  </script>
+  
