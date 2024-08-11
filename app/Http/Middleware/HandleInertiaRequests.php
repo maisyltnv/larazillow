92599532751039615b59043c9731b8ba7bcb0758
maisy <maisy.ltnl@kolaogroup.com>
2024-08-11 19:47:11 +0700

    update

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,7 +36,11 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            //
+            'flash'=>[
+                'success'=>$request->session()->get('success'),
+                'error'=>$request->session()->get('error'),
+                'warning'=>$request->session()->get('warning'),                
+            ]
         ]);
     }
 }
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,16 +1,27 @@
 <template>
    <Link href="/listing">Main Page</Link>
-   <Link href="/listing/create">Show Page</Link>
+   <Link href="/listing/create" >Show Page</Link>
+
+   <div v-if="flashSuccess" class="success">{{flashSuccess}}</div>
 
     
-
-
    <slot>Default</slot>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3';
-import {ref} from "vue";
+import {computed} from 'vue';
+import { Link ,usePage} from '@inertiajs/vue3';
 
+//flash success
+const page = usePage()
+const flashSuccess = computed(() => page.props.flash.success)
 
 </script>
+
+
+<style scoped>
+.success {
+   background-color: green;
+   color:white
+}
+</style>
