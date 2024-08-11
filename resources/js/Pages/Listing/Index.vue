59f9716c0e2a1c34206e6fbbf1eea9f6c00efd07
maisy <maisy.ltnl@kolaogroup.com>
2024-08-11 20:38:23 +0700

    delete

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,10 +1,15 @@
 <template>
     <div v-for="listing in listings" :key="listing.id">
-        <Link :href="`/listing/${listing.id}`">
-        <ListingAddress :listing="listing" />
-        </Link>
+        <div>
+            <Link :href="`/listing/${listing.id}`">
+            <ListingAddress :listing="listing" />
+            </Link>
+        </div>
         <div>
             <Link :href="`/listing/${listing.id}/edit`">Edit</Link>
+        </div>
+        <div>
+            <Link :href="`/listing/${listing.id}`" method="DELETE">Delete</Link>
         </div>
     </div>
 
