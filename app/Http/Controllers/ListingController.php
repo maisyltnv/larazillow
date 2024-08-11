234f9f5f46d34d511f85a90f1e33567da424bcca
maisy <maisy.ltnl@kolaogroup.com>
2024-08-11 20:13:30 +0700

    update

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -62,9 +62,11 @@ class ListingController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Listing $listing)
     {
-        //
+        return inertia('Listing/Edit', [
+            'listing'=>$listing
+        ]);
     }
 
     /**
